d50a7f17cfe49463b7a6a19610608910806e9842
KauePO <kaueperes2004@gmail.com>
2025-04-10 21:07:21 -0300

    Melhorias no projeto de framework Django

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -1,4 +1,6 @@
+from core.models import FeriadoModel
 from django.test import TestCase
+from datetime import datetime
 
 class NatalTest(TestCase):
     def setUp(self):
@@ -12,3 +14,14 @@ class NatalTest(TestCase):
 
     def test_template_natal(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.cadastro = FeriadoModel.objects.create(nome="Natal", dia=25, mes=12)
+
+    def test_created (self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_name (self):
+        feriado_no_banco = FeriadoModel.objects.first()
+        self.assertEqual(feriado_no_banco.nome, 'Natal')
--- feriados/core/views.py
+++ feriados/core/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from datetime import datetime
+from core.models import FeriadoModel
 
 def natal(request):
-    contexto = {'natal': False}
+    feriado_no_banco = FeriadoModel.objects.all()
+    contexto = {'feriado': None}
+
+
     data = datetime.now()
-    data = str(data.strftime("%m-%d"))
+    dia = data.strftime("%d")
+    mes = data.strftime("%m")
 
-    if (data == "12-25"):
-        contexto = {'natal': True}
+    dia_int = int(dia)
+    mes_int = int(mes)
 
+
+    for feriado in feriado_no_banco:
+        if (feriado.mes == mes_int and feriado.dia == dia_int): 
+            contexto['feriado'] = feriado.nome
+
+    if contexto['feriado'] is None:
+        contexto['feriado'] = "não é feriado"
+
+
+    #import ipdb; ipdb.set_trace()
     return render(request, 'natal.html', contexto)
